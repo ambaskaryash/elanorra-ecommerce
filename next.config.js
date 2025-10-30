ae8cd86731131ccc0c52c7b24472c42dd87b65ae
ambaskaryash <kimetsu119@gmail.com>
2025-10-30 14:58:25 +0530

    feat(api): add rewrites for script and tracking API endpoints

--- next.config.js
+++ next.config.js
@@ -142,6 +142,18 @@ const nextConfig = {
     }
     return config;
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/script.js",
+        destination: `${process.env.NEXT_PUBLIC_RYBBIT_HOST}/api/script.js`,
+      },
+      {
+        source: "/api/track",
+        destination: `${process.env.NEXT_PUBLIC_RYBBIT_HOST}/api/track`,
+      },
+    ];
+  },
 }
 
 export default withPWA(nextConfig);
